26eae62fe6b6d45a79f3487e2ae7cb4f7a472f1c
Fly <pasynkov_00@list.ru>
2017-07-07 16:06:58 +0500

    123

--- Sources/main.swift
+++ Sources/main.swift
@@ -11,8 +11,8 @@ import PerfectMustache
 class Handlers  {
     private let index = "/"
     private let developers = "/developers"
+    private let login = "/login"
     private let regist = "/registration"
-    
     
     struct mustacheHelper: MustachePageHandler {
         var values:  MustacheEvaluationContext.MapType
@@ -31,7 +31,7 @@ class Handlers  {
     func index(request: HTTPRequest, response: HTTPResponse) {
         var values = MustacheEvaluationContext.MapType()
         values["title"]="Главная страница"
-        values["regist"] = regist
+        values["regist"] = login
         values["index"] = index
         values["developers"] = developers
         mustacheRequest(request: request, response: response, handler: mustacheHelper(values: values),
@@ -40,22 +40,34 @@ class Handlers  {
     func developers(request: HTTPRequest, response: HTTPResponse) {
         var values = MustacheEvaluationContext.MapType()
         values["title"]="О разработчиках"
-        values["regist"] = regist
+        values["regist"] = login
         values["index"] = index
         values["developers"] = developers
         mustacheRequest(request: request, response: response, handler: mustacheHelper(values: values),
                         templatePath:"\(request.documentRoot)/developers.html")
+    
     }
     func registration(request: HTTPRequest, response: HTTPResponse) {
         var values = MustacheEvaluationContext.MapType()
         values["title"]="Регистрация"
-        values["regist"] = regist
+        values["regist"] = login
         values["index"] = index
         values["developers"] = developers
         mustacheRequest(request: request, response: response, handler: mustacheHelper(values: values),
                         templatePath:"\(request.documentRoot)/registration.html")
     }
     
+    func login(request: HTTPRequest, response: HTTPResponse) {
+        var values = MustacheEvaluationContext.MapType()
+        values["title"]="О разработчиках"
+        values["regist"] = login
+        values["index"] = index
+        values["developers"] = developers
+        mustacheRequest(request: request, response: response, handler: mustacheHelper(values: values),
+                        templatePath:"\(request.documentRoot)/login.html")
+        
+    }
+
 }
 //////////////swift build
 //.build/debug/perfect-server
@@ -64,10 +76,11 @@ class Handlers  {
 let handlers = Handlers()
 let server = HTTPServer()
 var routes = Routes()
-server.serverPort = 8181
+server.serverPort = 5050
 server.documentRoot="webroot"
 
 routes.add(method: .get, uri:"/", handler: handlers.index)
+routes.add(method: .get, uri:"/login", handler: handlers.login)
 routes.add(method: .get, uri:"/index", handler: handlers.index)
 routes.add(method: .get, uri:"/developers", handler: handlers.developers)
 routes.add(method: .get, uri:"/registration", handler: handlers.registration)
